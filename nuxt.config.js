c78ebe581f89b7fd8328d88421f19033b6f6054d
Yoshi <yoshis1057@gmail.com>
2020-11-27 17:05:16 +0900

    init

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+import theme from '@nuxt/content-theme-docs'
+
+export default theme({
+  docs: {
+    primaryColor: '#00CD81'
+  },
+  loading: { color: '#00CD81' },
+  i18n: {
+    locales: () => [{
+      code: 'en',
+      iso: 'en-US',
+      file: 'en-US.js',
+      name: 'English'
+    }],
+    defaultLocale: 'en'
+  }
+})
